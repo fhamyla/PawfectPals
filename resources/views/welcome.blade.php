fbbe58bcc2df4279479f95866f5359f897102949
fhamyla <fhamyla.devera@gmail.com>
2025-07-31 14:05:07 +0800

    modifying footer section making accessible to mobiel view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -463,9 +463,6 @@
                 <div class="footer-content">
                     <div class="footer-logo">
                         <div class="logo">
-                            <div class="logo-icon">
-                                <img src="/android-chrome-192x192.svg" alt="Pawfect Pals Logo" width="30" height="30">
-                            </div>
                             <span>Pawfect Pals</span>
                         </div>
                         <p class="footer-tagline">Your Purr-fect Cat Companion Awaits</p>
@@ -507,21 +504,31 @@
                     <div class="footer-social">
                         <h4>Follow Us</h4>
                         <div class="social-icons">
-                            <a href="#" class="social-icon">📷 Instagram</a>
-                            <a href="#" class="social-icon">🎵 TikTok</a>
-                            <a href="#" class="social-icon">📘 Facebook</a>
+                            <a href="https://www.linkedin.com/in/fhamyla/" class="social-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="16" height="16" fill="currentColor">
+                                    <path d="M512 96L127.9 96C110.3 96 96 110.5 96 128.3L96 511.7C96 529.5 110.3 544 127.9 544L512 544C529.6 544 544 529.5 544 511.7L544 128.3C544 110.5 529.6 96 512 96zM231.4 480L165 480L165 266.2L231.5 266.2L231.5 480L231.4 480zM198.2 160C219.5 160 236.7 177.2 236.7 198.5C236.7 219.8 219.5 237 198.2 237C176.9 237 159.7 219.8 159.7 198.5C159.7 177.2 176.9 160 198.2 160zM480.3 480L413.9 480L413.9 376C413.9 351.2 413.4 319.3 379.4 319.3C344.8 319.3 339.5 346.3 339.5 374.2L339.5 480L273.1 480L273.1 266.2L336.8 266.2L336.8 295.4L337.7 295.4C346.6 278.6 368.3 260.9 400.6 260.9C467.8 260.9 480.3 305.2 480.3 362.8L480.3 480z"/>
+                                </svg>
+                                LinkedIn
+                            </a>
+                            <a href="https://github.com/fhamyla" class="social-icon">
+                                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor">
+                                    <title>GitHub</title>
+                                    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+                                </svg>
+                                GitHub
+                            </a>
+                            <a href="https://fhamyla.netlify.app/" class="social-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="16" height="16" fill="currentColor">
+                                    <path d="M415.9 344L225 344C227.9 408.5 242.2 467.9 262.5 511.4C273.9 535.9 286.2 553.2 297.6 563.8C308.8 574.3 316.5 576 320.5 576C324.5 576 332.2 574.3 343.4 563.8C354.8 553.2 367.1 535.8 378.5 511.4C398.8 467.9 413.1 408.5 416 344zM224.9 296L415.8 296C413 231.5 398.7 172.1 378.4 128.6C367 104.2 354.7 86.8 343.3 76.2C332.1 65.7 324.4 64 320.4 64C316.4 64 308.7 65.7 297.5 76.2C286.1 86.8 273.8 104.2 262.4 128.6C242.1 172.1 227.8 231.5 224.9 296zM176.9 296C180.4 210.4 202.5 130.9 234.8 78.7C142.7 111.3 74.9 195.2 65.5 296L176.9 296zM65.5 344C74.9 444.8 142.7 528.7 234.8 561.3C202.5 509.1 180.4 429.6 176.9 344L65.5 344zM463.9 344C460.4 429.6 438.3 509.1 406 561.3C498.1 528.6 565.9 444.8 575.3 344L463.9 344zM575.3 296C565.9 195.2 498.1 111.3 406 78.7C438.3 130.9 460.4 210.4 463.9 296L575.3 296z"/>
+                                </svg>
+                                Portfolio
+                            </a>
                         </div>
                     </div>
                 </div>
                 
-                <div class="footer-separator">
-                    <span class="paw-separator">🐾</span>
-                    <span class="paw-separator">🐾</span>
-                    <span class="paw-separator">🐾</span>
-                </div>
-                
                 <div class="footer-bottom">
-                    <p>&copy; 2025 Pawfect Pals. All rights reserved.</p>
+                    <p>&copy; 2025 fhamyla. All rights reserved.</p>
                 </div>
             </div>
         </footer>
